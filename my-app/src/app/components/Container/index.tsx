29bb6ec651e42b59419f46caa8577ce5b5ac94f6
JorgeLucasPinheiro98 <jorgejdn@hotmail.com>
2025-09-27 06:58:59 -0300

    adicionei o formulario para enviar as tasks

--- my-app/src/app/components/Container/index.tsx
+++ my-app/src/app/components/Container/index.tsx
@@ -5,7 +5,7 @@ type ContainerProps = {
 export default function Container({children}:ContainerProps){
     return (
         <div className="flex justify-center items-center h-screen m-1">
-            <div className="bg-gray-500 p-2 px-3 m-1">
+            <div className="bg-gray-700 p-2 px-3 m-1 w-2/3">
                 <h1 className="text-center">Lista de Tarefas</h1>
                 {children}
             </div>
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,17 @@
-import Container from "./components";
+import Container from "./components/Container";
 
 export default function Home() {
   return (
     <div className="">
       <Container>
-        <p>Test</p>
+        <form action="">
+          <div className="flex justify-between items-center">
+            <label htmlFor="">Tarefa</label>
+            <input type="text" value="" className="bg-amber-50 m-1 w-full"/>
+            <button className="bg-gray-500 rounded-4xl">Enviar</button>
+          </div>
+        </form>
       </Container>
     </div>
-    )
+  );
 }
