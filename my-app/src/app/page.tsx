b0ec6e1acb4d7acdd29819559a3c9db1a4067efd
JorgeLucasPinheiro98 <jorgejdn@hotmail.com>
2025-09-28 05:48:53 -0300

    modifiquei o form

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,13 +2,13 @@ import Container from "./components/Container";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className=" bg-black text-white">
       <Container>
         <form action="">
           <div className="flex justify-between items-center">
-            <label htmlFor="">Tarefa</label>
-            <input type="text" value="" className="bg-amber-50 m-1 w-full"/>
-            <button className="bg-gray-500 rounded-4xl">Enviar</button>
+            <label htmlFor=""></label>
+            <input type="text" value="" placeholder="Digite sua Tarefa" className="bg-amber-50 m-1 w-full"/>
+            <button className="bg-gray-500 px-3">Enviar</button>
           </div>
         </form>
       </Container>
