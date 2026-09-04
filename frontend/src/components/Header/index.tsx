f86d56e3640454528b3b1bf68be94bdeca08810e
Jorge Lucas Pinheiro <jorgejdn@hotmail.com>
2026-09-04 15:49:36 -0300

    adicionando tarefas a aplicação

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,12 +1,12 @@
 import { Logo } from "../Logo";
-// import { Navegar } from "../Navegar";
+import { Navegar } from "../Navegar";
 import style from "./style.module.css"
 
 export function Header() {
     return (
         <header className={style.header}>
             <Logo/>
-            {/* <Navegar/> */}
+            <Navegar/>
         </header>
     )
 }
--- frontend/src/components/ListTask/index.tsx
+++ frontend/src/components/ListTask/index.tsx
@@ -1,27 +1,44 @@
+import React, { useState } from "react"
 import style from "./style.module.css"
 
 export function ListTask() {
+    const [tasks, setTasks] = useState<string[]>([]);
+    const [inputValue, setInputValue] = useState<string>("");
+
+    const handleAddItem = (e: React.FormEvent) => {
+        e.preventDefault();
+        if(inputValue.trim() === "") return
+        setTasks((prevTasks) => [...prevTasks, inputValue])
+        setInputValue("")
+    }
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value)
+    }
+    
+    const listTask = tasks.map(task => 
+        <section className={style.tasks}>
+            <p>{task}</p>
+            <button id={style.concluir}>Concluir</button>
+            <button>X</button>
+        </section>
+    )
+    
+    function addTask() {
+
+    }
+
     return (
         <div className={style.container}>
             <h1 className={style.title}>Lista de tarefas</h1>
                 <div className={style.back}>
-                <section className={style.addTask}>
+                <form onSubmit={handleAddItem}className={style.addTask}>
                     <label htmlFor="tarefa"></label>
-                    <input type="text" name="tarefa" placeholder="Digite uma tarefa"/>
-                    <button>Adicionar</button>
-                </section>
-                <section className={style.tasks}>
-                    <p>Trocar a senha do E-mail</p>
-                    <button id={style.concluir}>Concluir</button>
-                    <button>X</button>
-                </section>
-                <section className={style.tasks}>
-                    <p>Trocar a senha do E-mail</p>
-                    <button id={style.concluir}>Concluir</button>
-                    <button>X</button>
-                </section>
+                    <input type="text" name="tarefa" placeholder="Digite uma tarefa" value={inputValue} onChange={handleInputChange}/>
+                    <button onClick={addTask}>Adicionar</button>
+                </form>
+                {listTask}
             </div>
-            
         </div>
     )
 }
