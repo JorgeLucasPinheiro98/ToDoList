2209faf589db587a05908086526b47b82bdd62be
Jorge Lucas Pinheiro <jorgejdn@hotmail.com>
2026-09-07 09:45:06 -0300

    arrumando css das telas de login e register

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -3,14 +3,14 @@ import style from "./style.module.css"
 export function Login() {
     return (
         <div className={style.container}>
-            <form action="">
-                <h1>Login Page</h1>
+            <form action="" className={style.formLogin}>
+                <h1 className={style.textLogin}>Login Page</h1>
                 <label htmlFor="inputName"></label>
-                <input type="text" placeholder="Username" name="inputName"/>
+                <input type="text" placeholder="Username" name="inputName" className={style.elementsForm}/>
                 <label htmlFor="inputPassword"></label>
-                <input type="text" placeholder="Password" name="inputPassword"/>
-                <button>Login</button>
-                <a href="#">Esqueceu a senha?</a>
+                <input type="text" placeholder="Password" name="inputPassword" className={style.elementsForm}/>
+                <button className={style.elementsForm}>Login</button>
+                <a href="#" className={style.link}>Esqueceu a senha?</a>
             </form>
         </div>
     )
--- frontend/src/pages/register/index.tsx
+++ frontend/src/pages/register/index.tsx
@@ -1,5 +1,22 @@
+import style from "./style.module.css"
+
 export function Register() {
     return (
-        <h1>Register</h1>
+        <div className={style.container}>
+            <form action="" className={style.formRegister}>
+                <h1 className={style.textRegister}>Register Page</h1>
+                <label htmlFor="inputName"></label>
+                <input type="text" placeholder="Username" name="inputName" className={style.elementsForm}/>
+                <label htmlFor="inputEmail"></label>
+                <input type="text" placeholder="E-mail" name="inputEmail" className={style.elementsForm}/>
+                <label htmlFor="inputPassword"></label>
+                <input type="text" placeholder="Password" name="inputPassword" className={style.elementsForm}/>
+                <label htmlFor="repInputPassword"></label>
+                <input type="text" placeholder="Repeat Password" name="repeatInputPassword" className={style.elementsForm}/>
+                <button className={style.elementsForm}>Registrar</button>
+                <button className={style.elementsForm}>Limpar</button>
+                <a href="#">Fazer login</a>
+            </form>
+        </div>
     )
 }
