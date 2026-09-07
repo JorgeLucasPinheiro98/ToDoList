2ec802384dab02ad31199d6ce89d1360aeae5a16
Jorge Lucas Pinheiro <jorgejdn@hotmail.com>
2026-09-07 11:04:15 -0300

    criado o formulário da pagina de login

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,15 +1,45 @@
+import { useState, type ChangeEvent, type FormEvent } from "react"
 import style from "./style.module.css"
 
+interface IFormInputLogin {
+    userName: string
+    password: string
+}
+
 export function Login() {
+    const [formData, setFormData] = useState<IFormInputLogin>({
+        userName: "",
+        password: ""
+    })
+
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
+        const {name, value} = e.target
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    function handleSubmit (e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        console.log("dados do formulário:", formData)
+    }
+
+
+
     return (
         <div className={style.container}>
-            <form action="" className={style.formLogin}>
+            <form onSubmit={handleSubmit} className={style.formLogin}>
                 <h1 className={style.textLogin}>Login Page</h1>
+
                 <label htmlFor="inputName"></label>
-                <input type="text" placeholder="Username" name="inputName" className={style.elementsForm}/>
+                <input type="text" placeholder="Username" name="userName" className={style.elementsForm} value={formData.userName} onChange={handleChange}/>
+
                 <label htmlFor="inputPassword"></label>
-                <input type="text" placeholder="Password" name="inputPassword" className={style.elementsForm}/>
+                <input type="password" placeholder="Password" name="password" className={style.elementsForm} value={formData.password} onChange={handleChange}/>
+
                 <button className={style.elementsForm}>Login</button>
+
                 <a href="#" className={style.link}>Esqueceu a senha?</a>
             </form>
         </div>
