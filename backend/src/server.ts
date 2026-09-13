b236128db45faf047cfd17d0b8448e983650d1c0
Jorge Lucas Pinheiro <jorgejdn@hotmail.com>
2026-09-13 06:05:21 -0300

    criado as rotas de validação e senha para login

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
 import express, { type Request, type Response } from "express";
 import cors from "cors"
+import bcrypt from 'bcrypt';
 
 const app = express()
 const port = 3000;
 
-type IUser = {
+export type IUser = {
     userName: string;
     email: string;
     password: string;
@@ -20,17 +21,30 @@ app.get('/users', (req: Request, res: Response) => {
     res.status(200).json(users);
 });
 
-app.post('/users', (req: Request, res: Response) => {
+app.post('/users', async (req: Request, res: Response) => {
     const data = req.body
+    const hash = await bcrypt.hash(data.password, 4)
     const user = {
         userName: data.userName,
         email: data.email,
-        password: data.password,
+        password: hash,
     }
-    console.log(user, new Date().getDate())
-    res.status(201).json(user);
+    console.log(user, new Date().getTime())
     users.push(user)
+    res.status(201).json(user);
 });
+
+app.post("/login", async (req: Request, res: Response) => {
+    const data = req.body
+    const user = users.find((user) => user.userName === data.userName)
+    const loginSucesso = user ? await bcrypt.compare(data.password, user.password) : false
+
+    if(!loginSucesso) {
+        return res.status(401).json({error: "Credenciais Invalidas"})
+    }
+
+    return res.status(200).json({message: "Login Realidado"})
+})
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
--- backend/test/server.test.ts
+++ backend/test/server.test.ts
@@ -1,4 +1,6 @@
-test("deve criar um post", async () => {
+import type { IUser } from "../src/server.ts"
+
+test("deve criar um Usuario", async () => {
     const user = {
         userName: "testuser",
         email: "test@example.com",
@@ -11,8 +13,27 @@ test("deve criar um post", async () => {
         body: JSON.stringify(user)
     })
 
+    const data: IUser | any = await response.json()
+
+    expect(response.status).toBe(201)
+    expect(data.userName).toBe(user.userName)
+    expect(data.email).toBe(user.email)
+})
+
+test("deve fazer um Login", async () => {
+    const userLogin = {
+        userName: "testuser",
+        password: "123",
+    }
+
+    const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(userLogin)
+    })
+
     const data = await response.json()
     console.log(data)
 
-    expect(response.status).toBe(201)
+    expect(response.status).toBe(200)
 })
